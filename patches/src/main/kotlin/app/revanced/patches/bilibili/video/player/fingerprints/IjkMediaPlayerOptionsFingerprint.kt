15752e2584b0bad3289d3ecf6ec2de8980c268c5
Your Name <you@example.com>
2024-11-18 19:18:50 +0800

    1

--- patches/src/main/kotlin/app/revanced/patches/bilibili/video/player/fingerprints/IjkMediaPlayerOptionsFingerprint.kt
+++ patches/src/main/kotlin/app/revanced/patches/bilibili/video/player/fingerprints/IjkMediaPlayerOptionsFingerprint.kt
@@ -3,7 +3,7 @@ package app.revanced.patches.bilibili.video.player.fingerprints
 import app.revanced.patcher.fingerprint.MethodFingerprint
 
 object IjkMediaPlayerOptionsFingerprint : MethodFingerprint(
-    strings = listOf("enable-decoder-switch", "enable-use-https-when-forbidden"),
+    strings = listOf("enable-decoder-switch", "enable-h265"),
     parameters = listOf(),
     returnType = "V"
 )
